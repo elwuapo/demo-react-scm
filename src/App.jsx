c8159f7971816abe0860ed493bff0775938f9787
Nicolas Rivera <43177162+elwuapo@users.noreply.github.com>
2025-05-22 15:21:33 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,59 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-	const helloEs = 'Hello, world'
-	const helloEn = 'Hola mundo'
+	const helloEs = "Hello, world";
+	const helloEn = "Hola mundo";
 
-	const [isEn, setIsEn] = useState(true)
-	const [employees, setEmployees] = useState([])
+	const [isEn, setIsEn] = useState(true);
+	const [employees, setEmployees] = useState([]);
 
-	const buttonChange = <button onClick={() => setIsEn(!isEn)}>cambiar idioma</button>
+	const buttonChange = (
+		<button onClick={() => setIsEn(!isEn)}>cambiar idioma</button>
+	);
 
 	const getEmployees = useCallback(async () => {
-		axios.get('https://62c5f952134fa108c260aa1b.mockapi.io/employees')
+		axios
+			.get("https://680d1242c47cb8074d8f7878.mockapi.io/users")
 			.then((response) => {
-				console.log('entre!')
-				setEmployees(response.data)
-			}).catch((error) => {
-				console.error(error.response)
+				console.log("entre!");
+				setEmployees(response.data);
 			})
-	}, [])
+			.catch((error) => {
+				console.error(error.response);
+			});
+	}, []);
 
 	useEffect(() => {
-		getEmployees()
-	}, [getEmployees])
+		getEmployees();
+	}, [getEmployees]);
 
 	return (
 		<div>
-			<h1>
-				{ isEn? helloEn: helloEs }
-			</h1>
+			<h1>{isEn ? helloEn : helloEs}</h1>
 
 			{buttonChange}
 
-			{ employees.map((employee, index) => 
+			{employees.map((employee, index) => (
 				<div key={index}>
-					position: { index + 1 } <br />
-					firstname : {employee.firstname} <br />
-					lastname : {employee.lastname} <br />
-					email : {employee.email} <br /><br />
+					<img
+						width={100}
+						height={100}
+						style={{ borderRadius: 100 }}
+						src={employee.avatar}
+						alt="avatar"
+					/>{" "}
+					<br />
+					id: {employee.id} <br />
+					position: {index + 1} <br />
+					firstname : {employee.first_name} <br />
+					lastname : {employee.last_name} <br />
+					email : {employee.email} <br />
+					<br />
 				</div>
-			)}
+			))}
 		</div>
-	)
-}
+	);
+};
 
-export { App }
+export { App };
